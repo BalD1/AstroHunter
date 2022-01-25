e11cb3c7843b977334a96f1c85c6a6461ee1eb50
Florian <florianhusson@hotmail.fr>
2022-01-25 17:19:20 +0100

    send

--- HA-GJ-2022/Assets/Scripts/Characters/Enemies/Meteor.cs
+++ HA-GJ-2022/Assets/Scripts/Characters/Enemies/Meteor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Meteor : Enemies
 {
-
+    [SerializeField] private GameObject sign;
     [SerializeField] private float lifeTime;
     [SerializeField] private float timeBeforeLaunch = 1;
 
---
+++ HA-GJ-2022/Assets/Scripts/MeteorSign.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSign : MonoBehaviour
+{
+    [HideInInspector] public Vector2 meteorPosition;
+
+    void Update()
+    {
+        this.transform.position = meteorPosition;
+
+        Vector3 pos = GameManager.Instance.getMainCamera().WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
