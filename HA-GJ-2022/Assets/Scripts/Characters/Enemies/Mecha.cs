435c7de8b4a570ef2ff3fe00705d7764c2e8aa77
Florian <florianhusson@hotmail.fr>
2022-01-25 19:35:09 +0100

    added mecha

---
+++ HA-GJ-2022/Assets/Scripts/Characters/Enemies/Mecha.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mecha : Enemies
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        if (GameManager.Instance.GameState == GameManager.GameStates.InGame)
+        {
+            base.Update();
+        }
+    }
+}
