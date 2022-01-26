66386a5962de7d565876ed5bf92a6cf5373d04b8
Florian <florianhusson@hotmail.fr>
2022-01-26 12:27:38 +0100

    parallex

---
+++ HA-GJ-2022/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector2 offset;
+
+    [SerializeField] private Vector2 clamp;
+
+    private void FixedUpdate()
+    {
+        Vector2 dest = (Vector2)target.position + offset;
+        Vector2 smooth = Vector2.Lerp((Vector2)this.transform.position, dest, smoothSpeed);
+        smooth.x = Mathf.Clamp(smooth.x, -clamp.x, clamp.x);
+        smooth.y = Mathf.Clamp(smooth.y, -clamp.y, clamp.y);
+        this.transform.position = smooth;
+    }
+}
